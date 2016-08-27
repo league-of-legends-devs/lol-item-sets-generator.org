cc46d7c8a5cf8c3ca82017e5f6aa58a7ffa6e9fe
Ilshidur <nicolas-coutin@outlook.com>
2016-08-27 16:05:02 +0200

    Fix indent.

--- app/lib/collections/guestbook_entries.js
+++ app/lib/collections/guestbook_entries.js
@@ -1,67 +1,67 @@
 GuestbookEntries = new Mongo.Collection('guestbook_entries');
 
 var formSchema = {
-	text: {
-		type: String,
-		label: "Review text",
-		max: 256
-	}
+  text: {
+    type: String,
+    label: "Review text",
+    max: 256
+  }
 };
 GuestbookEntriesFormSchema = new SimpleSchema(formSchema);
 
 GuestbookEntriesSchema = new SimpleSchema(Object.assign(formSchema, {
-	author: {
-		type: String,
-		label: "Author",
-		max: 100
-	},
-	number: {
-		type: Number,
-		label: "Number",
-		max: 1000
-	},
-	answer: {
-		type: String,
-		label: "Answer",
-		optional: true
-	},
-	date: {
-		type: Date,
-		label: "Date"
-	},
-	approved: {
-		type: Boolean,
-		label: "Approved"
-	}
+  author: {
+    type: String,
+    label: "Author",
+    max: 100
+  },
+  number: {
+    type: Number,
+    label: "Number",
+    max: 1000
+  },
+  answer: {
+    type: String,
+    label: "Answer",
+    optional: true
+  },
+  date: {
+    type: Date,
+    label: "Date"
+  },
+  approved: {
+    type: Boolean,
+    label: "Approved"
+  }
 }));
 GuestbookEntries.attachSchema(GuestbookEntriesSchema);
 
 if (Meteor.isServer) {
-	GuestbookEntries.allow({
-		insert: function (userId, doc) {
-			return false;
-		},
+  GuestbookEntries.allow({
+    insert: function (userId, doc) {
+      return false;
+    },
 
-		update: function (userId, doc, fieldNames, modifier) {
-			return false;
-		},
+    update: function (userId, doc, fieldNames, modifier) {
+      return false;
+    },
 
-		remove: function (userId, doc) {
-			return false;
-		}
-	});
+    remove: function (userId, doc) {
+      return false;
+    }
+  });
 
-	GuestbookEntries.deny({
-		insert: function (userId, doc) {
-			return true;
-		},
+  GuestbookEntries.deny({
+    insert: function (userId, doc) {
+      return true;
+    },
 
-		update: function (userId, doc, fieldNames, modifier) {
-			return true;
-		},
+    update: function (userId, doc, fieldNames, modifier) {
+      return true;
+    },
 
-		remove: function (userId, doc) {
-			return true;
-		}
-	});
+    remove: function (userId, doc) {
+      return true;
+    }
+  });
 }
