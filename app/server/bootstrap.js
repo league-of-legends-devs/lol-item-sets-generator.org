bdd3044390569152023a72b50516b3110ea2d013
Ilshidur <nicolas-coutin@outlook.com>
2016-08-29 13:49:00 +0200

    Replace ian:accounts-ui-bootstrap-3 with accounts-ui.

--- app/server/bootstrap.js
+++ app/server/bootstrap.js
@@ -1,13 +1,13 @@
 Meteor.startup(function () {
-  Accounts.onCreateUser(function (options, user) {
-    user.profile = {};
-    // Create a empty array to avoid the Exception while invoking method 'adminCheckAdmin'
-    // see : https://github.com/yogiben/meteor-admin/issues/260
-    user.emails = [];
-    // The first user is an admin
-    if (Meteor.users.find().count() === 0) {
-      user.roles = ['admin'];
-    }
-    return user;
-  });
+  // Accounts.onCreateUser(function (options, user) {
+  //   user.profile = {};
+  //   // Create a empty array to avoid the Exception while invoking method 'adminCheckAdmin'
+  //   // see : https://github.com/yogiben/meteor-admin/issues/260
+  //   user.emails = [];
+  //   // The first user is an admin
+  //   if (Meteor.users.find().count() === 0) {
+  //     user.roles = ['admin'];
+  //   }
+  //   return user;
+  // });
 });
