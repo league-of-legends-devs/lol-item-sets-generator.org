df8ea33db8e715b7a29bcec68fe1d322c50234e5
Ilshidur <nicolas-coutin@outlook.com>
2016-09-13 01:41:39 +0200

    ES6 vars instead of "var".

--- app/server/methods.js
+++ app/server/methods.js
@@ -6,17 +6,17 @@ Meteor.methods({
   'server/postEntry': function (doc) {
     this.unblock();
     check(doc, GuestbookEntriesFormSchema);
-    var user = Meteor.users.findOne({ _id: this.userId });
+    const user = Meteor.users.findOne({ _id: this.userId });
     if (!user.services.twitter) {
       throw new Meteor.Error(403, `You can't post if you are not authenticated with Twitter.`, `Can't post if not authenticated`);
     }
-    var username = `@${user.services.twitter.screenName}`;
-    var userAlreadyPosted = GuestbookEntries.findOne({ author: username }) !== undefined;
+    const username = `@${user.services.twitter.screenName}`;
+    const userAlreadyPosted = GuestbookEntries.findOne({ author: username }) !== undefined;
     if (userAlreadyPosted) {
       throw new Meteor.Error(403, `You can't post an entry twice. If you're having a problem, please contact us through our Twitter.`, `Can't post twice`);
     }
     doc.author = username;
-    var entriesCount = GuestbookEntries.find().count() + 1;
+    const entriesCount = GuestbookEntries.find().count() + 1;
     doc.number = entriesCount;
     doc.answer = '';
     doc.date = Date.now();
@@ -28,7 +28,7 @@ Meteor.methods({
   },
   'server/approveEntry': function (entryId) {
     this.unblock();
-    var user = Meteor.users.findOne({ _id: this.userId });
+    const user = Meteor.users.findOne({ _id: this.userId });
     if (!Roles.userIsInRole(user, ['admin'])) {
       throw new Meteor.Error(403, `You can't approve an entry if you're not an admin.`, `Can't approve if not admin`);
     }
