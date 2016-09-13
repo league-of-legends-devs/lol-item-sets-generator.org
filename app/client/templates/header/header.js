ddc81cbea39d285ef4222810323be819f9611d9c
Ilshidur <nicolas-coutin@outlook.com>
2016-09-13 02:23:47 +0200

    Temporarily disable login buttons.

--- app/client/templates/header/header.js
+++ app/client/templates/header/header.js
@@ -12,7 +12,8 @@ Template.Header.events({
 Template.Header.helpers({
   styles: styles,
   showLoginButtons: () => {
-    return Template.instance().showLoginButtons.get();
+    return false;
+    // return Template.instance().showLoginButtons.get();
   }
 });
 
@@ -20,21 +21,21 @@ Template.Header.helpers({
 /* Header: Lifecycle Hooks */
 /*****************************************************************************/
 Template.Header.onCreated(function () {
-  let template = Template.instance();
-  template.showLoginButtons = new ReactiveVar(true);
-  Meteor.call('server/getConfig', 'showLoginButtons', function (err, response) {
-    if (!err) {
-      template.showLoginButtons.set(response.value == 'true');
-    } else {
-      /*$.notify({
-      	message: err,
-        icon: 'glyphicon glyphicon-warning-sign'
-      },{
-      	type: 'danger'
-      });*/
-      console.error(err);
-    }
-  });
+  // let template = Template.instance();
+  // template.showLoginButtons = new ReactiveVar(true);
+  // Meteor.call('server/getConfig', 'showLoginButtons', function (err, response) {
+  //   if (!err) {
+  //     template.showLoginButtons.set(response.value == 'true');
+  //   } else {
+  //     $.notify({
+  //     	message: err,
+  //       icon: 'glyphicon glyphicon-warning-sign'
+  //     },{
+  //     	type: 'danger'
+  //     });
+  //     console.error(err);
+  //   }
+  // });
 });
 
 Template.Header.onRendered(function () {
