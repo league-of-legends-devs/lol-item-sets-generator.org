29b5753a91b49b5c1c5a19f57224acfb2c23b227
Ilshidur <nicolas-coutin@outlook.com>
2016-09-20 11:48:54 +0200

    Remove the "rootUrl" config.

--- app/lib/config.js
+++ app/lib/config.js
@@ -1,7 +1,5 @@
 AppConfig = {
   appName: 'LoL item sets generator',
 
-  appDescription: 'Download the best -generated- League of Legends recommended items.',
-
-  rootUrl: process.env.ROOT_URL
+  appDescription: 'Download the best -generated- League of Legends recommended items.'
 };
